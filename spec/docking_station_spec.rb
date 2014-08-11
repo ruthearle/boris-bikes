334cc9fa61400c14264e2edd6f969267f925b67e
ruthearle <rrearle@gmail.com>
2014-08-11 19:00:13 +0100

    test for docking station when full

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,7 +2,7 @@ require 'docking_station'
 require 'bike'
 
 describe DockingStation do
-  let(:station) {DockingStation.new}
+  let(:station) {DockingStation.new(:capacity => 20)}
   let(:bike) {Bike.new}
 
   it "should accept a bike" do
@@ -15,6 +15,17 @@ describe DockingStation do
     station.dock(bike)
     station.release(bike)
     expect(station.bike_count).to eq(0)
+  end
+
+  it "should know when it's full" do
+    expect(station.full?).to be false
+    20.times { station.dock(Bike.new) }
+    expect(station.full?).to be true
+  end
+
+  it "should not accept a bike if it's full" do
+    20.times { station.dock(Bike.new) }
+    expect(lambda { station.dock(bike) }).to raise_error(RuntimeError )
   end
 
 end
