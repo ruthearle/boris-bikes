e70ec25b61ece0943962c6a27d392c70b0dc125c
Jerome Pratt <jerome.pratt@outlook.com>
2014-08-13 20:18:42 +0100

    Adding and testing abilty for van to collect only broke bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,9 +1,15 @@
-require 'docking_station'
+require './lib/docking_station'
+
 
 describe DockingStation do
+
+	it_behaves_like 'a bike container'
+
   let(:station) {DockingStation.new(:capacity => 123)}
 
-  it 'should allow setting default capaacity on initialising' do
+  it 'should allow setting default capacity on initialising' do
     expect(station.capacity).to eq(123)
   end
+
+
 end
