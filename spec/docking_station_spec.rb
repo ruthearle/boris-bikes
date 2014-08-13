a5bc6158f0704adb98f69eb4852412f69faeac83
ruthearle <rrearle@gmail.com>
2014-08-13 14:16:38 +0100

    changed test for docking station capacity at initialisation

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,9 @@
 require 'docking_station'
 
 describe DockingStation do
-  let(:station) {DockingStation.new(:capacity => 20)}
-  let(:bike) {Bike.new}
+  let(:station) {DockingStation.new(:capacity => 123)}
 
-
-
-
-
-
+  it 'should allow setting default capaacity on initialising' do
+    expect(station.capacity).to eq(123)
+  end
 end
