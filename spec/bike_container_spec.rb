d310d8fcfc6e8ca48d6625c3818e13bd52f715e4
Jerome Pratt <jerome.pratt@outlook.com>
2014-08-13 11:10:40 +0100

    Moving tests from docking_station_spec to bike_container_spec-release

--- spec/bike_container_spec.rb
+++ spec/bike_container_spec.rb
@@ -15,4 +15,12 @@ describe BikeContainer do
     # now we expect the holder to have 1 bike
     expect(holder.bike_count).to eq(1)
   end
+
+    it "should release a bike" do
+    holder.dock(bike)
+    holder.release(bike)
+    expect(holder.bike_count).to eq(0)
+  end
+
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,17 +4,7 @@ describe DockingStation do
   let(:station) {DockingStation.new(:capacity => 20)}
   let(:bike) {Bike.new}
 
-  it "should accept a bike" do
-   expect(station.bike_count).to eq(0)
-   station.dock(bike)
-   expect(station.bike_count).to eq(1)
-  end
 
-  it "should release a bike" do
-    station.dock(bike)
-    station.release(bike)
-    expect(station.bike_count).to eq(0)
-  end
 
   def fill_station(station)
     20.times { station.dock(Bike.new) }
