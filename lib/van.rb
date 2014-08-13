5d48a26d58859204264ecfb470c05600c1f29097
ruthearle <rrearle@gmail.com>
2014-08-13 17:15:18 +0100

    Initialised default capacity for van

--- lib/van.rb
+++ lib/van.rb
@@ -2,17 +2,10 @@ require_relative 'bike_container'
 
 class Van
 
+  include BikeContainer
 
-
-
-
-
-
-
-
-
-
-
-
+  def initialize(options = {})
+    self.capacity = options.fetch(:capacity, capacity)
+  end
 
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,2 +1,11 @@
 require 'van'
 
+describe Van do
+
+  let(:van) {Van.new(:capacity => 123)}
+  it 'should be able to set a default capacity on initialisation' do
+    expect(van.capacity).to eq (123)
+  end
+
+end
+
