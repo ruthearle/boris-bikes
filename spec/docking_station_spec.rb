885e727410a5ab8de24108c226e8132ebb8f2cb2
Jerome Pratt <jerome.pratt@outlook.com>
2014-08-13 14:05:36 +0100

    Moving last test from docking_station_spec to bike_container_spec - list of available bikes

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -6,13 +6,7 @@ describe DockingStation do
 
 
 
-  it "should provide the list of available bikes" do
-    working_bike, broken_bike = Bike.new, Bike.new
-    broken_bike.break!
-    station.dock(working_bike)
-    station.dock(broken_bike)
-    expect(station.available_bikes).to eq([working_bike])
-  end
+
 
 
 end
