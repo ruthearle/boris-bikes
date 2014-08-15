c990bc0b5a6440464c9a2944f1f8b92d9efadc87
ruthearle <rrearle@gmail.com>
2014-08-15 11:23:52 +0100

    wrote a test for van to initialize at a specific location

--- lib/van.rb
+++ lib/van.rb
@@ -8,17 +8,13 @@ class Van
     self.capacity = options.fetch(:capacity, capacity)
   end
 
-  def retrieve(bike)
-  	broken_bikes << broken_bike
-  end
-
   def collect(station)
-  	station.bikes.each do |bike|
-  		if bike.broken?
-  		  self.dock(bike)
-  		  station.release(bike)
-  		 end 
-  	end
+    station.bikes.each do |bike|
+      if bike.broken?
+        self.dock(bike)
+        station.release(bike)
+      end
+    end
   end
 
 
